08350063b0f2a8c730d9b611c3e927cf20216469
a21604343 <56267823+a21604343@users.noreply.github.com>
2022-03-25 16:44:05 +0000

    activity arranjada, otimização do codigo na MainActivity.kt  e adicionada lista para guardar historico

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -11,40 +11,78 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private val TAG = MainActivity::class.java.simpleName
+
+    var listaHistorico : MutableList<String> = mutableListOf()
+
     override fun onCreate(savedInstanceState: Bundle?) {
-        Log.i(TAG,"onCreated invoke")
+        Log.i(TAG, "onCreated invoke")
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 
 
-
-    override fun onStart(){
+    override fun onStart() {
         super.onStart()
 
-        binding.button1.setOnClickListener{
-            Log.i(TAG, "Click no Butao 1")
-            if(binding.textVisor.text == "0"){
-                binding.textVisor.text = "1"
-            }else{
-                binding.textVisor.append("1")
+        binding.button1.setOnClickListener {onClickSymbol("1")}
+        binding.button2.setOnClickListener {onClickSymbol("2")}
+        binding.button3.setOnClickListener {onClickSymbol("3")}
+        binding.button4.setOnClickListener {onClickSymbol("4")}
+        binding.button5.setOnClickListener {onClickSymbol("5")}
+        binding.button6.setOnClickListener {onClickSymbol("6")}
+        binding.button7.setOnClickListener {onClickSymbol("7")}
+        binding.button8.setOnClickListener {onClickSymbol("8")}
+        binding.button9.setOnClickListener {onClickSymbol("9")}
+        binding.button0.setOnClickListener {onClickSymbol("0")}
+        binding.buttonAdition.setOnClickListener {onClickSymbol("+")}
+        binding.buttonMinus.setOnClickListener {onClickSymbol("-")}
+        binding.buttonDivide.setOnClickListener {onClickSymbol("/")}
+        binding.buttonQuestionMark.setOnClickListener {onClickSymbol("*")}
+        binding.buttonClear.setOnClickListener {onClickSymbol("C")}
+        binding.buttonSmaller.setOnClickListener {onClickSymbol("P")}
+        binding.buttonEquals.setOnClickListener {onClickEquals()}
+
+
+
+
+
+
+
+    }
+    private fun onClickSymbol(symbol: String){
+
+        Log.i(TAG, "Click no Butao $symbol")
+        if(symbol == "C"){
+            binding.textVisor.text = ""
+        }
+        else if (symbol == "P"){
+            binding.textVisor.text.subSequence(0,binding.textVisor.text.length-2)
+        }else{
+            if (binding.textVisor.text == "0") {
+                binding.textVisor.text = symbol
+            } else {
+                binding.textVisor.append(symbol)
             }
         }
 
-        binding.buttonAdition.setOnClickListener{
-            Log.i(TAG, "Click no butao +")
-            binding.textVisor.append("+")
-        }
 
-        binding.buttonEquals.setOnClickListener{
-            Log.i(TAG, "Click no butao =")
-            val expression = ExpressionBuilder(
-                binding.textVisor.text.toString()
-            ).build()
-            binding.textVisor.text = expression.evaluate().toString()
-            Log.i(TAG, "O resultado da expressão é ${binding.textVisor.text}")
-        }
+
+
 
 
     }
+
+    private fun onClickEquals(){
+
+        Log.i(TAG, "Click no butao =")
+        val expression = ExpressionBuilder(
+            binding.textVisor.text.toString()
+        ).build()
+        binding.textVisor.text = expression.evaluate().toString()
+        listaHistorico.add("$expression = ${binding.textVisor.text}")
+        Log.i(TAG, "O resultado da expressão é ${binding.textVisor.text}")
+
+    }
 }
+
